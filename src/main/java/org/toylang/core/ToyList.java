5dbbaf270e890571b1847ec4ebddebb9203e9cbe
Brad <bradley.wood@uoit.net>
2017-10-06 15:26:01 -0400

    format, leave index unchecked

--- src/main/java/org/toylang/core/ToyList.java
+++ src/main/java/org/toylang/core/ToyList.java
@@ -12,23 +12,28 @@ public class ToyList extends ToyObject implements List {
     public ToyList() {
         list = new ArrayList<>();
     }
+
     @Hidden
     public ToyList(ToyObject[] objects) {
         this();
         list.addAll(Arrays.asList(objects));
     }
+
     public List<ToyObject> getList() {
         return list;
     }
+
     @Hidden
     public ToyList(List<Object> obj) {
         this();
         addAll(obj);
     }
+
     @Override
     public ToyObject getType() {
         return TYPE;
     }
+
     @Override
     public int size() {
         return list.size();
@@ -38,44 +43,44 @@ public class ToyList extends ToyObject implements List {
     public boolean isEmpty() {
         return list.isEmpty();
     }
+
     @Override
     public boolean contains(Object o) {
         return list.contains(o);
     }
+
     @Override
     public Iterator iterator() {
         return list.iterator();
     }
+
     @Override
     public ToyObject add(ToyObject obj) {
         list.add(obj);
         return this;
     }
+
     @Override
     public ToyObject set(ToyObject index, ToyObject obj) {
         if (index instanceof ToyInt) {
-            int idx = index.toInt();
-            if(idx < list.size() && idx >= 0) {
-                return list.set(idx , obj);
-            }
+            return list.set(index.toInt(), obj);
         }
         return super.set(index, obj);
     }
+
     @Override
     public ToyObject get(ToyObject obj) {
         if (obj instanceof ToyInt) {
-            int idx = ((ToyInt) obj).toInt();
-            if(idx < list.size()) {
-                return list.get(idx);
-            }
+            return list.get(obj.toInt());
         }
         return super.get(obj);
     }
+
     @Hidden
     @Override
     public Object[] toArray() {
         Object[] obja = new Object[list.size()];
-        for(int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             obja[i] = list.get(i).toObject();
         }
         return obja;
@@ -83,8 +88,8 @@ public class ToyList extends ToyObject implements List {
 
     @Override
     public boolean add(Object o) {
-        if(o instanceof ToyObject) {
-            return list.add((ToyObject)o);
+        if (o instanceof ToyObject) {
+            return list.add((ToyObject) o);
         } else {
             return list.add(new ToyObject(o));
         }
@@ -94,47 +99,54 @@ public class ToyList extends ToyObject implements List {
     public boolean remove(Object o) {
         return list.remove(o);
     }
+
     @Override
     public boolean addAll(Collection c) {
         int size = list.size();
-        for(Object o : c) {
+        for (Object o : c) {
             add(o);
         }
         return size != list.size();
     }
+
     @Override
     public boolean addAll(int index, Collection c) {
         int size = list.size();
         Iterator it = c.iterator();
-        for(int i = index; i < c.size(); i++) {
+        for (int i = index; i < c.size(); i++) {
             add(i, it.next());
         }
         return size != list.size();
     }
+
     @Override
     public void clear() {
         list.clear();
     }
+
     @Override
     public Object get(int index) {
         return list.get(index);
     }
+
     @Override
     public Object set(int index, Object element) {
-        if(element instanceof ToyObject) {
+        if (element instanceof ToyObject) {
             return list.set(index, (ToyObject) element);
         } else {
             return list.set(index, new ToyObject(element));
         }
     }
+
     @Override
     public void add(int index, Object element) {
-        if(element instanceof ToyObject) {
+        if (element instanceof ToyObject) {
             list.add(index, (ToyObject) element);
         } else {
             list.add(index, new ToyObject(element));
         }
     }
+
     @Override
     public Object remove(int index) {
         return list.remove(index);
@@ -149,11 +161,13 @@ public class ToyList extends ToyObject implements List {
     public int lastIndexOf(Object o) {
         return list.lastIndexOf(o);
     }
+
     @Hidden
     @Override
     public ListIterator listIterator() {
         return list.listIterator();
     }
+
     @Hidden
     @Override
     public ListIterator listIterator(int index) {
@@ -174,24 +188,29 @@ public class ToyList extends ToyObject implements List {
     public boolean removeAll(Collection c) {
         return list.removeAll(c);
     }
+
     @Override
     public boolean containsAll(Collection c) {
         return list.containsAll(c);
     }
+
     @Hidden
     @Override
     public Object[] toArray(Object[] a) {
         return toArray();
     }
+
     @Hidden
     @Override
     public Object toObject() {
         return list;
     }
+
     @Override
     public String toString() {
         return list.toString();
     }
+
     @Hidden
     @Override
     public boolean equals(Object o) {
@@ -203,6 +222,7 @@ public class ToyList extends ToyObject implements List {
 
         return list != null ? list.equals(toyList.list) : toyList.list == null;
     }
+
     @Hidden
     @Override
     public int hashCode() {
