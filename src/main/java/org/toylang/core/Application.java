e2992121fb7a150788612dafc5f4fdcc641b512f
Brad <bradley.wood@uoit.net>
2017-10-15 13:59:49 -0400

    basic tests

--- src/main/java/org/toylang/core/Application.java
+++ src/main/java/org/toylang/core/Application.java
@@ -3,36 +3,38 @@ package org.toylang.core;
 import org.toylang.antlr.Errors;
 import org.toylang.antlr.ToyParser;
 import org.toylang.antlr.ToyTree;
-import org.toylang.antlr.ast.Statement;
 import org.toylang.compiler.Compiler;
+import org.toylang.test.Assert;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Application {
 
     public static void main(String[] args) {
         try {
             if (args.length >= 1) {
-                if(args[0].endsWith(".tl")) {
+                if (args[0].endsWith(".tl")) {
                     compileAndRun(args[0], Arrays.copyOfRange(args, 1, args.length));
+                } else if (args[0].equals("-test")) {
+                    compileAndTest("/test/org/toylang/test/");
                 } else {
-                    compile(args[0]);
+                    compile(args[0], true);
                 }
             } else {
-                compile("/src/main/toylang/toylang/lang/");
+                compile("/src/main/toylang/toylang/lang/", true);
 
                 System.out.println("----------------------------------------------");
 
-                compile("/scripts/test/");
+                compile("/scripts/test/", true);
             }
 
         } catch (IOException | NoSuchMethodException | ClassNotFoundException | IllegalAccessException | InvocationTargetException e) {
@@ -40,44 +42,82 @@ public class Application {
         }
     }
 
+    public static void compileAndTest(String path) throws IOException, ClassNotFoundException {
+        path = path.replace("/", "\\");
+        File file = new File(path);
+
+        HashMap<String, byte[]> classes = compile(path, true);
+
+        int numTests = 0;
+        AtomicInteger fails = new AtomicInteger();
+
+        if (Errors.getErrorCount() == 0) {
+            ByteClassLoader cl = new ByteClassLoader(null, Application.class.getClassLoader(), classes);
+            if (classes != null) {
+                for (String s : classes.keySet()) {
+                    Class<?> clazz = cl.loadClass(s);
+                    for (Method method : clazz.getDeclaredMethods()) {
+                        if (method.getName().toLowerCase().contains("test") && method.getParameterCount() == 0) {
+                            try {
+                                numTests++;
+                                method.setAccessible(true);
+                                method.invoke(null);
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                e.getCause().printStackTrace();
+                                fails.getAndIncrement();
+                            }
+                            Assert.errors.forEach(error -> {
+                                error.printStackTrace();
+                                fails.getAndIncrement();
+                            });
+                            Assert.errors.clear();
+                        }
+                    }
+                }
+            }
+        }
+        System.err.println("All tests completed, " + (numTests - fails.get()) + "/" + numTests + " passed.");
+    }
+
     public static void compileAndRun(String path, String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         path = path.replace("/", "\\");
         File file = new File(path);
-        ToyParser parser = new ToyParser(file.getAbsolutePath());
-        ToyTree tree = parser.parse();
-        String name = file.getName().replace(".tl", "");
-        Compiler compiler = new Compiler(file.getAbsolutePath(), name, tree);
-        HashMap<String, byte[]> classes = compiler.compile(false);
-        if(Errors.getErrorCount() == 0) {
+
+        HashMap<String, byte[]> classes = compile(path, false);
+        if (Errors.getErrorCount() == 0) {
             ByteClassLoader cl = new ByteClassLoader(null, Application.class.getClassLoader(), classes);
-            for (String s : classes.keySet()) {
-                if(file.getAbsolutePath().endsWith(s.replace(".","\\") + ".tl")) {
-                    Class<?> app = cl.loadClass(s);
-                    Method m = app.getMethod("main", String[].class);
-                    m.invoke(null, (Object) args);
-                    break;
+            if (classes != null) {
+                for (String s : classes.keySet()) {
+                    if (file.getAbsolutePath().endsWith(s.replace(".", "\\") + ".tl")) {
+                        Class<?> app = cl.loadClass(s);
+                        Method m = app.getMethod("main", String[].class);
+                        m.invoke(null, (Object) args);
+                        break;
+                    }
                 }
             }
         }
     }
 
-    public static void compile(String path) throws IOException {
+    public static HashMap<String, byte[]> compile(String path, boolean save) throws IOException {
         Compiler compiler;
         ToyParser parser;
         ToyTree tree;
         File file = new File(new File(".").getCanonicalPath(), path);
-
+        HashMap<String, byte[]> classes = null;
         for (File f : Objects.requireNonNull(file.listFiles())) {
-            if(!f.getAbsolutePath().endsWith(".tl"))
+            if (!f.getAbsolutePath().endsWith(".tl"))
                 continue;
             parser = new ToyParser(f.getPath());
             tree = parser.parse();
             compiler = new Compiler(f.getAbsolutePath(), f.getName().replace(".tl", ""), tree);
-            compiler.compile();
+            classes = compiler.compile(save);
         }
-        if(Errors.getErrorCount() == 0) {
+        if (Errors.getErrorCount() == 0) {
             System.out.println("Compilation Completed Successfully!");
+            return classes;
         }
+        return null;
     }
 
     public static class ByteClassLoader extends ClassLoader {
---
+++ src/main/java/org/toylang/test/Assert.java
@@ -0,0 +1,27 @@
+package org.toylang.test;
+
+import java.util.LinkedList;
+
+public class Assert {
+
+    public static LinkedList<AssertionError> errors = new LinkedList<>();
+
+    public static void fail() {
+        fail("");
+    }
+
+    public static void fail(String msg) {
+        errors.add(new AssertionError(msg));
+    }
+
+    public static void assertTrue(boolean b) {
+        if (!b)
+            fail("value is not true");
+    }
+
+    public static void assertEqual(Object a, Object b) {
+        if (!a.equals(b)) {
+            fail(a + " is not equal to " + b);
+        }
+    }
+}
