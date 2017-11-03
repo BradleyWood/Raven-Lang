a4e343678f7dae43c7939ede0d72bfb2fc2c5788
BradleyWood <bradley.wood@uoit.net>
2017-11-03 13:01:32 -0400

    for control accept parenthesis

--- src/main/java/org/toylang/antlr/visitor/ForVisitor.java
+++ src/main/java/org/toylang/antlr/visitor/ForVisitor.java
@@ -20,13 +20,13 @@ public class ForVisitor extends ToyLangBaseVisitor<For> {
         Block body = new Block();
         Block after = new Block();
 
-        if (ctx.COLON() != null) {
+        if (ctx.forControl().COLON() != null) {
             Random r = new Random();
             VarDecl init = new VarDecl(new QualifiedName(String.valueOf(r.nextInt())), new Literal(new TInt(0)));
-            QualifiedName name = new QualifiedName(ctx.IDENTIFIER().getText());
+            QualifiedName name = new QualifiedName(ctx.forControl().IDENTIFIER().getText());
             body.append(new VarDecl(name, null));
 
-            Expression iterable = ctx.expression(0).accept(ExpressionVisitor.INSTANCE);
+            Expression iterable = ctx.forControl().expression(0).accept(ExpressionVisitor.INSTANCE);
             QualifiedName iterableName;
             if (!(iterable instanceof QualifiedName)) {
                 iterableName = new QualifiedName(String.valueOf(r.nextInt()));
@@ -41,11 +41,11 @@ public class ForVisitor extends ToyLangBaseVisitor<For> {
             after.append(new BinOp(init.getName(), Operator.ASSIGNMENT, new BinOp(init.getName(), Operator.ADD, new Literal(new TInt(1)))));
 
             return new For(init, condition, body, after);
-        } else if (ctx.range() != null) {
-            QualifiedName name = new QualifiedName(ctx.IDENTIFIER().getText());
-            boolean inc = ctx.range().inc() != null;
+        } else if (ctx.forControl().range() != null) {
+            QualifiedName name = new QualifiedName(ctx.forControl().IDENTIFIER().getText());
+            boolean inc = ctx.forControl().range().inc() != null;
 
-            Range range = ctx.range().accept(RangeVisitor.INSTANCE);
+            Range range = ctx.forControl().range().accept(RangeVisitor.INSTANCE);
 
             VarDecl init = new VarDecl(name, range.getStart());
 
@@ -57,20 +57,20 @@ public class ForVisitor extends ToyLangBaseVisitor<For> {
             after.append(new BinOp(name, Operator.ASSIGNMENT, new BinOp(name, Operator.ADD, new Literal(new TInt(inc ? 1 : -1)))));
 
             return new For(init, condition, body, after);
-        } else if(ctx.statement() != null) {
+        } else if (ctx.statement() != null) {
             Statement init = new Expression();
             Expression condition = new Literal(TBoolean.TRUE);
-            if(ctx.init != null)
-                init = ctx.init.accept(ExpressionVisitor.INSTANCE);
-            else if(ctx.decl != null)
-                init = ctx.decl.accept(VarDeclVisitor.INSTANCE);
-            if(ctx.cond != null)
-                condition = ctx.cond.accept(ExpressionVisitor.INSTANCE);
+            if (ctx.forControl().init != null)
+                init = ctx.forControl().init.accept(ExpressionVisitor.INSTANCE);
+            else if (ctx.forControl().decl != null)
+                init = ctx.forControl().decl.accept(VarDeclVisitor.INSTANCE);
+            if (ctx.forControl().cond != null)
+                condition = ctx.forControl().cond.accept(ExpressionVisitor.INSTANCE);
 
-            int size = ctx.paramList().param() != null ? ctx.paramList().param().size() : 0;
+            int size = ctx.forControl().paramList().param() != null ? ctx.forControl().paramList().param().size() : 0;
             Expression[] af = new Expression[size];
-            for(int i = 0; i < af.length; i++) {
-                af[i] = ctx.paramList().param(i).accept(ExpressionVisitor.INSTANCE);
+            for (int i = 0; i < af.length; i++) {
+                af[i] = ctx.forControl().paramList().param(i).accept(ExpressionVisitor.INSTANCE);
             }
             body.append(ctx.statement().accept(StatementVisitor.INSTANCE));
             Arrays.stream(af).forEach(after::append);
