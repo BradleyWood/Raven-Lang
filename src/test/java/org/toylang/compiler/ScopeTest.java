c9de3046804586f8fe6978c77774d569842a1b2f
Brad <bradley.wood@uoit.net>
2017-11-27 18:49:47 -0500

    scope tests

--- src/test/java/org/toylang/compiler/ScopeTest.java
+++ src/test/java/org/toylang/compiler/ScopeTest.java
@@ -1,6 +1,7 @@
 package org.toylang.compiler;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ScopeTest {
@@ -8,18 +9,88 @@ public class ScopeTest {
     private final Scope scope = new Scope();
 
     @Test
-    public void beginScope() {
+    public void testScopeCreation() {
+        Assert.assertEquals(0, scope.count());
+        scope.beginScope();
+        Assert.assertEquals(1, scope.count());
+        scope.endScope();
+        Assert.assertEquals(0, scope.count());
+        scope.beginScope();
+        scope.beginScope();
+        scope.beginScope();
+        scope.endScope();
+        Assert.assertEquals(2, scope.count());
+        scope.endScope();
+        scope.endScope();
+        Assert.assertEquals(0, scope.count());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testScopeUnderflow() {
+        scope.beginScope();
+        scope.endScope();
+        scope.endScope();
     }
 
     @Test
-    public void endScope() {
+    public void testVars1() {
+        scope.beginScope();
+
+        scope.putVar("test");
+        Assert.assertEquals(0, scope.findVar("test"));
+
+        scope.endScope();
     }
 
     @Test
-    public void putVar() {
+    public void testVars2() {
+        scope.beginScope();
+
+        scope.putVar("test");
+        Assert.assertEquals(0, scope.findVar("test"));
+        scope.putVar("testing");
+        Assert.assertEquals(1, scope.findVar("testing"));
+
+        scope.endScope();
     }
 
     @Test
-    public void findVar() {
+    public void testVars3() {
+        scope.beginScope();
+
+        scope.putVar("test");
+        Assert.assertEquals(0, scope.findVar("test"));
+
+        scope.beginScope();
+        scope.putVar("testing");
+        Assert.assertEquals(1, scope.findVar("testing"));
+        scope.endScope();
+
+        Assert.assertEquals(0, scope.findVar("test"));
+
+        Assert.assertEquals(-1, scope.findVar("testing"));
+
+        scope.endScope();
+    }
+
+    @Test
+    public void testVarOverload() {
+        scope.clear();
+        scope.beginScope();
+
+        scope.putVar("test");
+
+        Assert.assertEquals(0, scope.findVar("test"));
+
+        scope.beginScope();
+
+        Assert.assertEquals(0, Errors.getErrorCount());
+        scope.putVar("test");
+        Assert.assertEquals(1, Errors.getErrorCount());
+
+        scope.endScope();
+
+        scope.endScope();
+        scope.clear();
     }
 }
