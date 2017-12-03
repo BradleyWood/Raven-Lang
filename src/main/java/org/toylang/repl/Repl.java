4123f2f79bae28f8953de4febfcbe3ab2216ea54
Brad <bradley.wood@uoit.net>
2017-12-02 22:43:13 -0500

    REPL implementation

---
+++ src/main/java/org/toylang/repl/Repl.java
@@ -0,0 +1,108 @@
+package org.toylang.repl;
+
+import org.toylang.antlr.Modifier;
+import org.toylang.antlr.Operator;
+import org.toylang.antlr.StatementParser;
+import org.toylang.antlr.ast.*;
+import org.toylang.compiler.ClassMaker;
+import org.toylang.compiler.Errors;
+import org.toylang.core.ByteClassLoader;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Read-eval-print-loop utility
+ */
+public class Repl {
+
+    private final ByteClassLoader classLoader = new ByteClassLoader(null, Repl.class.getClassLoader(), new HashMap<>());
+    private final LinkedList<Import> imports = new LinkedList<>();
+    private Class parent = null;
+    private int counter = 0;
+
+
+    public void exec(String line) {
+        Class cl = build(line);
+        if (cl != null) {
+            try {
+                parent = cl;
+                cl.getDeclaredMethod("exec").invoke(null);
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Class build(String line) {
+        List<Statement> statementList = StatementParser.parseStatements(line);
+
+        if (Errors.getErrorCount() > 0) {
+            Errors.reset();
+            return null;
+        }
+
+        LinkedList<VarDecl> staticVars = new LinkedList<>();
+        LinkedList<Fun> functions = new LinkedList<>();
+        LinkedList<Statement> statements = new LinkedList<>();
+
+        for (Statement statement : statementList) {
+            if (statement instanceof Import) {
+                if (!imports.contains(statement))
+                    imports.add((Import) statement);
+            } else if (statement instanceof VarDecl) {
+                VarDecl decl = (VarDecl) statement;
+                staticVars.add(decl);
+                statements.add(new BinOp(decl.getName(), Operator.ASSIGNMENT, decl.getInitialValue()));
+            } else if (statement instanceof Fun) {
+                functions.add((Fun) statement);
+            } else {
+                statements.add(statement);
+            }
+        }
+
+        QualifiedName superClass = new QualifiedName("java", "lang", "Object");
+        if (parent != null) {
+            superClass = new QualifiedName(parent.getName().split("\\."));
+        }
+
+        String name = "Repl" + counter++;
+        ClassDef def = new ClassDef(new Modifier[]{Modifier.PUBLIC}, new QualifiedName("repl"), name, superClass,
+                new QualifiedName[0], new ArrayList<>());
+
+        ClassMaker maker = new ClassMaker(def, imports.stream().map(Import::getName).collect(Collectors.toList()));
+        staticVars.forEach(maker::addStaticFields);
+        maker.addStaticMethods(createExec(statements));
+
+        functions.forEach(maker::addStaticMethods);
+
+        Fun clinit = new Fun(new QualifiedName("<clinit>"), new Block(), null, null);
+        maker.addStaticMethods(clinit);
+
+        maker.make();
+        byte[] bytes = maker.getBytes();
+        if (Errors.getErrorCount() > 0) {
+            Errors.printErrors();
+            return null;
+        }
+
+        String clName = "repl." + name;
+
+        classLoader.addDef(clName, bytes);
+        try {
+            return classLoader.loadClass(clName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static Fun createExec(List<Statement> statements) {
+        return new Fun(new QualifiedName("exec"),
+                new Block(statements.toArray(new Statement[statements.size()])), new Modifier[]{Modifier.PUBLIC}, new String[0]);
+    }
+}
