3b03379177becbf283c4f7f17eb4108d9131da1f
BradleyWood <bradley.wood@uoit.net>
2017-12-21 23:00:39 -0500

    test for loop syntax

--- src/test/java/org/toylang/antlr/SyntaxTest.java
+++ src/test/java/org/toylang/antlr/SyntaxTest.java
@@ -51,5 +51,58 @@ public class SyntaxTest {
         testSyntax("while ;", false);
         // no statement or block
         testSyntax("while (true)", false);
+
+        testSyntax("while", false);
+    }
+
+    @Test
+    public void testFor() {
+        testSyntax(" for ;; ;", true);
+        testSyntax(" for ;; {}", true);
+        testSyntax(" for (;;) ;", true);
+        testSyntax(" for (;;) {}", true);
+        testSyntax(" for (i = 0; i < 10; i += 2) ;", true);
+        testSyntax(" for (i = 0; i < 10; i += 2) {}", true);
+        testSyntax(" for i = 0; i < 10; i += 2 ;", true);
+        testSyntax(" for i = 0; i < 10; i += 2 {}", true);
+
+        testSyntax("for ;", false);
+        testSyntax("for (;;)", false);
+        testSyntax("for (;;;)", false);
+        testSyntax("for", false);
+    }
+
+    @Test
+    public void testForRanges() {
+        testSyntax("for i range 0 to 0 ;", true);
+        testSyntax("for (i range 0 to 0) ;", true);
+        testSyntax("for i range 0 upto 0 ;", true);
+        testSyntax("for i range 0 downto 0 ;", true);
+        testSyntax("for i range 0 to 0 {}", true);
+
+        testSyntax("for range 0 to 0 ;", false);
+        testSyntax("for i range (0 to 0) ;", false);
+
+        testSyntax("for i (range 0 to 0) ;", false);
+        testSyntax("for i range (0 to 0) ;", false);
+        testSyntax("for i a range 0 to 0 ;", false);
+        testSyntax("for a : b range 0 to 0 ;", false);
+        testSyntax("for ;; range 0 to 0 ;", false);
+        testSyntax("for range 0 to 0 {}", false);
+    }
+
+    @Test
+    public void testForEach() {
+        testSyntax("for a : b ;", true);
+        testSyntax("for a : b {}", true);
+        testSyntax("for ((a : b)) ;", true);
+        testSyntax("for ((a : b)) {}", true);
+        testSyntax("for (a : b) {}", true);
+
+        testSyntax("for a : b range a to b;", false);
+        testSyntax("for (range a to b) a : b ;", false);
+        testSyntax("for : ;;", false);
+        testSyntax("for a: ;;", false);
+        testSyntax("for :b ;;", false);
     }
 }
