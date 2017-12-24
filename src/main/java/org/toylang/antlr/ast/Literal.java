d552295d0a6c4bdd10aeced515e373d6c061a887
BradleyWood <bradley.wood@uoit.net>
2017-12-23 22:37:33 -0500

    add toString() for literal ast node

--- src/main/java/org/toylang/antlr/ast/Literal.java
+++ src/main/java/org/toylang/antlr/ast/Literal.java
@@ -33,6 +33,11 @@ public class Literal extends Expression {
     }
 
     @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
