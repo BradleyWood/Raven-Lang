974ca35da4a21023196fdc0882f652dab16fd9fd
BradleyWood <bradley.wood@uoit.net>
2018-01-23 19:37:26 -0500

    simply

--- src/main/java/org/toylang/antlr/visitor/ExpressionVisitor.java
+++ src/main/java/org/toylang/antlr/visitor/ExpressionVisitor.java
@@ -22,35 +22,25 @@ public class ExpressionVisitor extends ToyLangBaseVisitor<Expression> {
             for (String s : qn.getNames()) {
                 expr = new Call(expr, new QualifiedName("getField"), new QualifiedName(s));
             }
-            expr.setLineNumber(ctx.expression(0).start.getLine());
         } else if (ctx.expression().size() == 1 && ctx.funCall() != null) {
             expr = ctx.funCall().accept(FunCallVisitor.INSTANCE);
             ((Call) expr).setPrecedingExpr(ctx.expression(0).accept(ExpressionVisitor.INSTANCE));
-            expr.setLineNumber(ctx.funCall().start.getLine());
         } else if (ctx.literal() != null) {
             expr = ctx.literal().accept(LiteralVisitor.INSTANCE);
-            expr.setLineNumber(ctx.literal().start.getLine());
         } else if (ctx.dict() != null) {
             expr = ctx.dict().accept(DictDefVisitor.INSTANCE);
-            expr.setLineNumber(ctx.dict().start.getLine());
         } else if (ctx.list() != null) {
             expr = ctx.list().accept(ArrayDefVisitor.INSTANCE);
-            expr.setLineNumber(ctx.list().start.getLine());
         } else if (ctx.listIdx() != null) {
             expr = ctx.listIdx().accept(ListIndexVisitor.INSTANCE);
-            expr.setLineNumber(ctx.listIdx().start.getLine());
         } else if (ctx.slice() != null) {
             expr = ctx.slice().accept(SliceVisitor.INSTANCE);
-            expr.setLineNumber(ctx.slice().start.getLine());
         } else if (ctx.funCall() != null) {
             expr = ctx.funCall().accept(FunCallVisitor.INSTANCE);
-            expr.setLineNumber(ctx.funCall().start.getLine());
         } else if (ctx.qualifiedName() != null) {
             expr = ctx.qualifiedName().accept(QualifiedNameVisitor.INSTANCE);
-            expr.setLineNumber(ctx.qualifiedName().start.getLine());
         } else if (ctx.varAssignment() != null) {
             expr = ctx.varAssignment().accept(AssignmentVisitor.INSTANCE);
-            expr.setLineNumber(ctx.varAssignment().start.getLine());
         } else if (ctx.expression().size() == 1) {
             expr = ctx.expression(0).accept(ExpressionVisitor.INSTANCE);
             if (ctx.SUB() != null) {
@@ -63,15 +53,17 @@ public class ExpressionVisitor extends ToyLangBaseVisitor<Expression> {
             } else if (ctx.NOT() != null) {
                 expr = new BinOp(null, Operator.NOT, expr);
             }
-            expr.setLineNumber(ctx.expression(0).start.getLine());
         } else if (ctx.expression().size() == 2) {
             Expression left = ctx.expression(0).accept(ExpressionVisitor.INSTANCE);
             Expression right = ctx.expression(1).accept(ExpressionVisitor.INSTANCE);
             Operator operator = getOperator(ctx);
             expr = new BinOp(left, operator, right);
-            expr.setLineNumber(ctx.expression(0).start.getLine());
         } else {
             Errors.put("Unsupported expression: " + ctx.getText());
+        }
+
+        if (expr != null) {
+            expr.setLineNumber(ctx.start.getLine());
         }
 
         return expr;
