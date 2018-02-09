da32e61150772d98350be051b7587a20791da765
BradleyWood <bradley.wood@uoit.net>
2018-02-09 17:47:57 -0500

    settings functions need to be static

--- src/main/java/org/toylang/util/Settings.java
+++ src/main/java/org/toylang/util/Settings.java
@@ -6,23 +6,23 @@ public class Settings {
 
     private static final HashMap<String, Object> SETTINGS = new HashMap<>();
 
-    public Object get(final String key) {
+    public static Object get(final String key) {
         return SETTINGS.get(key);
     }
 
-    public int getInt(final String key) {
+    public static int getInt(final String key) {
         return (int) SETTINGS.get(key);
     }
 
-    public boolean getBoolean(final String key) {
+    public static boolean getBoolean(final String key) {
         return (boolean) SETTINGS.get(key);
     }
 
-    public String getString(final String key) {
+    public static String getString(final String key) {
         return (String) SETTINGS.get(key);
     }
 
-    public void set(String key, Object value) {
+    public static void set(String key, Object value) {
         SETTINGS.put(key, value);
     }
 
