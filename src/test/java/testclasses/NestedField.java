4dab72dee554cda04a5623a2bcf2866deb71bdd4
BradleyWood <bradley.wood@uoit.net>
2018-03-23 22:33:38 -0400

    test class for nested fields

---
+++ src/test/java/testclasses/NestedField.java
@@ -0,0 +1,28 @@
+package testclasses;
+
+/**
+ * to test nested field access
+ * ex: a = instance.nestedField.nestedField.value
+ * ex: instance.nestedField.nestedField.value = "abc"
+ */
+public class NestedField {
+
+    public static final NestedField instance = new NestedField(
+            new NestedField(new NestedField(null, "c"), "b"), "a");
+
+    public NestedField nestedField;
+    public String value;
+
+    public NestedField(final NestedField nestedField, String value) {
+        this.nestedField = nestedField;
+        this.value = value;
+    }
+
+    public NestedField getNestedField() {
+        return nestedField;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
