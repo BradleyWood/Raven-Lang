10c6b1ad7b636c1db7b9b04b571f0861a63eacb7
BradleyWood <bradley.wood@uoit.net>
2018-03-27 19:09:51 -0400

    impl try-catch-finally parser

--- src/main/java/org/raven/antlr/visitor/TryCatchFinallyVisitor.java
+++ src/main/java/org/raven/antlr/visitor/TryCatchFinallyVisitor.java
@@ -1,9 +1,27 @@
 package org.raven.antlr.visitor;
 
 import org.raven.antlr.RavenBaseVisitor;
+import org.raven.antlr.RavenParser;
+import org.raven.antlr.ast.Block;
+import org.raven.antlr.ast.TryCatchFinally;
 
-public class TryCatchFinallyVisitor extends RavenBaseVisitor {
+public class TryCatchFinallyVisitor extends RavenBaseVisitor<TryCatchFinally> {
 
+    private TryCatchFinallyVisitor() {
+    }
 
+    @Override
+    public TryCatchFinally visitTryCatchFinally(RavenParser.TryCatchFinallyContext ctx) {
+        Block body = ctx.block(0).accept(BlockVisitor.INSTANCE);
+        Block handler = ctx.block(1).accept(BlockVisitor.INSTANCE);
+        Block finallyBlock = null;
 
+        if (ctx.block().size() == 3) {
+            finallyBlock = ctx.block(2).accept(BlockVisitor.INSTANCE);
+        }
+
+        return new TryCatchFinally(body, handler, finallyBlock);
+    }
+
+    public static final TryCatchFinallyVisitor INSTANCE = new TryCatchFinallyVisitor();
 }
