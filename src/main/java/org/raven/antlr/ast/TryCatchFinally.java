ca8e63527200e417593cb4d308d30fe6ad02305c
BradleyWood <bradley.wood@uoit.net>
2018-03-27 18:51:32 -0400

    ast node for try-catch-finally

---
+++ src/main/java/org/raven/antlr/ast/TryCatchFinally.java
@@ -0,0 +1,45 @@
+package org.raven.antlr.ast;
+
+import java.util.Objects;
+
+public class TryCatchFinally extends Statement {
+
+    private final Block body;
+    private final Block handler;
+    private final Block finallyBlock;
+
+    public TryCatchFinally(Block body, Block handler, Block finallyBlock) {
+        this.body = body;
+        this.handler = handler;
+        this.finallyBlock = finallyBlock;
+    }
+
+    public Block getBody() {
+        return body;
+    }
+
+    public Block getHandler() {
+        return handler;
+    }
+
+    public Block getFinallyBlock() {
+        return finallyBlock;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        TryCatchFinally that = (TryCatchFinally) o;
+        return Objects.equals(body, that.body) &&
+                Objects.equals(handler, that.handler) &&
+                Objects.equals(finallyBlock, that.finallyBlock);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), body, handler, finallyBlock);
+    }
+}
