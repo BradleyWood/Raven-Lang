b6fed48577d3312f280b277f315e8594d751e00d
BradleyWood <bradley.wood@uoit.net>
2018-04-22 19:30:23 -0400

    test inheritance from java classes

---
+++ src/test/java/testclasses/Person.java
@@ -0,0 +1,32 @@
+package testclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private List<Person> children = new ArrayList<>();
+    private final String name;
+    private final int age;
+
+    public Person(final String name, final int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void addChild(final Person person) {
+        this.children.add(person);
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+}
