837acbcaeb30a7510014ceaecf7d07c7229fa2ef
BradleyWood <bradley.wood@uoit.net>
2018-04-23 18:53:27 -0400

    test coercion of fields inherited from java classes

---
+++ src/test/java/testclasses/Container.java
@@ -0,0 +1,22 @@
+package testclasses;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Container {
+
+    public List list;
+
+    public Container(List list) {
+        this.list = list;
+        System.out.println("a");
+    }
+
+    public Container(Object[] oa) {
+        this(Arrays.asList(oa));
+    }
+
+    public Object get(int idx) {
+        return list.get(idx);
+    }
+}
