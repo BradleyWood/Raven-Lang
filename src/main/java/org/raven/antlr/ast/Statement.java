81b74c91955402219c7c83c48adbb9cc06562d84
BradleyWood <bradley.wood@uoit.net>
2018-04-23 15:46:29 -0400

    provide documentation of Statement node

--- src/main/java/org/raven/antlr/ast/Statement.java
+++ src/main/java/org/raven/antlr/ast/Statement.java
@@ -14,26 +14,52 @@ public class Statement extends AST {
 
     protected int lineNumber = -1;
 
+    /**
+     * Returns any annotations that this statement may have.
+     *
+     * @return The List of Annotations
+     */
     public List<Annotation> getAnnotations() {
         return annotations;
     }
 
+    /**
+     * Set the string of tokens that comprise this statement
+     *
+     * @param text The string of tokens
+     */
     public void setText(final String text) {
         this.text = text;
     }
 
+    /**
+     * @return the string of tokens that comprise this statement
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Add a new annotation to this statement
+     *
+     * @param annotation The annotation to add
+     */
     public void addAnnotation(final Annotation annotation) {
         annotations.add(annotation);
     }
 
+    /**
+     *
+     * @return The line number that this statement was written on
+     */
     public int getLineNumber() {
         return lineNumber;
     }
 
+    /**
+     *
+     * @param lineNumber The line number
+     */
     public void setLineNumber(final int lineNumber) {
         this.lineNumber = lineNumber;
     }
