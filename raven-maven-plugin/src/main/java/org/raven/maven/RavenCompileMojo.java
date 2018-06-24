4b01a3edcd019db3184686c82b29d73ae5993c84
BradleyWood <bradley.wood@uoit.net>
2018-06-24 01:47:57 -0400

    include dependencies in classpath for compiler plugin

--- raven-maven-plugin/src/main/java/org/raven/maven/RavenCompileMojo.java
+++ raven-maven-plugin/src/main/java/org/raven/maven/RavenCompileMojo.java
@@ -5,7 +5,7 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.raven.error.Error;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.raven.error.Errors;
 import org.raven.util.Settings;
 import org.raven.util.Utility;
@@ -13,7 +13,7 @@ import org.raven.util.Utility;
 import java.io.IOException;
 import java.util.List;
 
-@Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE)
+@Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE)
 public class RavenCompileMojo extends AbstractMojo {
 
     @Parameter(name = "sourceDirectory", defaultValue = "${project.basedir}\\src\\main\\raven")
@@ -29,7 +29,7 @@ public class RavenCompileMojo extends AbstractMojo {
     public void execute() throws CompilationFailureException {
         Settings.set("OUT", outputDirectory);
         try {
-            Utility.compile(sourceDirectory, true);
+            Utility.compile(sourceDirectory, classpath, true);
         } catch (IOException e) {
             throw new CompilationFailureException(e.getMessage());
         } finally {
