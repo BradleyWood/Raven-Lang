0cb1e3fe7d9aba53d7d60051546a56ac9b44cf75
BradleyWood <bradley.wood@uoit.net>
2018-06-23 22:27:51 -0400

    raven-maven-plugin

---
+++ raven-maven-plugin/src/main/java/org/raven/maven/RavenMojo.java
@@ -0,0 +1,12 @@
+package org.raven.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "compile")
+public class RavenMojo extends AbstractMojo {
+
+    @Override
+    public void execute() {
+    }
+}
