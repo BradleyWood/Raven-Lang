7c191b2ff628accded437d9c4940568ce58f1750
BradleyWood <bradley.wood@uoit.net>
2018-07-08 11:20:39 -0400

    test output produced by when visitor

---
+++ raven-compiler/src/test/java/org/raven/antlr/visitor/WhenVisitorTest.java
@@ -0,0 +1,55 @@
+package org.raven.antlr.visitor;
+
+import org.junit.Test;
+import org.raven.antlr.ast.*;
+import org.raven.core.wrappers.TBoolean;
+import org.raven.core.wrappers.TInt;
+import org.raven.core.wrappers.TNull;
+
+import static org.raven.antlr.RuleTester.testStatement;
+
+public class WhenVisitorTest {
+
+    @Test
+    public void testWhen() {
+        final Case[] cases = new Case[2];
+        cases[0] = new Case(new Literal(new TInt(10)), new Block(new Literal(new TInt(100))));
+        cases[1] = new Case(new QualifiedName("b"), new Block(new QualifiedName("b")));
+
+        final When when = new When(new QualifiedName("x"), cases, new Block(new Literal(new TInt(1000))));
+
+        testStatement(WhenVisitor.INSTANCE, "when (x) { 10->100 \n b->b \n else -> 1000 }", when);
+    }
+
+    @Test
+    public void testWhenWithBlock() {
+        final Case[] cases = new Case[2];
+        cases[0] = new Case(new Literal(new TInt(10)), new Block(new Literal(TNull.NULL)));
+
+        final When when = new When(new QualifiedName("x"), cases, new Block(new Literal(TBoolean.TRUE)));
+
+        testStatement(WhenVisitor.INSTANCE, "when (x) { 10->{} \n else -> { true } }", when);
+    }
+
+    @Test
+    public void testWhenWithMultiStmtBlock() {
+        final Case[] cases = new Case[2];
+        cases[0] = new Case(new Literal(new TInt(10)), new Block(new Literal(TNull.NULL)));
+
+        final Expression expr = new Literal(new TInt(10));
+        expr.setPop(true);
+        final When when = new When(new QualifiedName("x"), cases, new Block(expr, new Literal(TBoolean.TRUE)));
+
+        testStatement(WhenVisitor.INSTANCE, "when (x) { 10->{} \n else -> { 10\n true } }", when);
+    }
+
+    @Test
+    public void testWhenEmptyBlock() {
+        final Case[] cases = new Case[2];
+        cases[0] = new Case(new Literal(new TInt(10)), new Block(new Literal(TNull.NULL)));
+
+        final When when = new When(new QualifiedName("x"), cases, new Block(new Literal(TNull.NULL)));
+
+        testStatement(WhenVisitor.INSTANCE, "when (x) { 10->{} \n else -> {} }", when);
+    }
+}
